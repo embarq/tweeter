416094a5b7edb757803388619d6988d50efb11f2
embarq <embarq.dev@gmail.com>
2020-09-05 16:59:33 +0300

    remove preact

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,8 @@
 export default function IndexPage() {
   return (
     <div>
-      <div class="py-20">
-        <h1 class="text-5xl text-center text-accent-1">
+      <div className="py-20">
+        <h1 className="text-5xl text-center text-accent-1">
           Next.js + Tailwind CSS
         </h1>
       </div>
