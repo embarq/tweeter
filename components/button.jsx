02a4c2e58afb87f5a344beaccc99790f7982723b
embarq <embarq.dev@gmail.com>
2020-09-20 22:28:43 +0300

    convert button styles to tailwind components

--- components/button.jsx
+++ components/button.jsx
@@ -1,8 +1,8 @@
 import classNames from 'classnames'
 import Loader from './loader'
 
-const defaultStyles = 'flex items-center shadow bg-indigo-600 hover:bg-indigo-500 focus:shadow-outline focus:outline-none text-white py-2 px-4 rounded'
-const clearStyles = 'text-gray-600 hover:text-gray-700 hover:bg-gray-100 focus:shadow-outline focus:outline-none py-2 px-4 rounded'
+const defaultStyles = 'btn hover:bg-indigo-500 focus:shadow-outline focus:outline-none'
+const clearStyles = 'btn-clear hover:text-gray-700 hover:bg-gray-100 focus:shadow-outline focus:outline-none'
 
 export default function Button({className, children, kind, loading, ...props}) {
   const buttonStyle = kind === 'clear' ? clearStyles : defaultStyles
