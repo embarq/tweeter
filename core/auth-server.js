b42dbeac3b81612684dcbc543e4d54f3b9b818c7
embarq <embarq.dev@gmail.com>
2020-09-25 11:50:23 +0300

    impl firebase-admin's token verification

---
+++ core/auth-server.js
@@ -0,0 +1,23 @@
+import * as admin from 'firebase-admin'
+
+export const verifyIdToken = (token) => {
+  if (!admin.apps.length) {
+    const firebasePrivateKey = process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY
+    admin.initializeApp({
+      credential: admin.credential.cert({
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+        clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
+        // https://stackoverflow.com/a/41044630/1332513
+        privateKey: firebasePrivateKey.replace(/\\n/g, '\n'),
+      }),
+      databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+    })
+  }
+
+  return admin
+    .auth()
+    .verifyIdToken(token)
+    .catch((error) => {
+      throw error
+    })
+}
