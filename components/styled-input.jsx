386b1ccf25a76bb5cd2bc0e2d16d9d4b71423f8a
embarq <embarq.dev@gmail.com>
2020-12-09 12:49:59 +0200

    set sm text size

--- components/styled-input.jsx
+++ components/styled-input.jsx
@@ -1,6 +1,6 @@
 import { cloneElement, Children } from 'react'
 
-const defaultStyles = 'bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-indigo-400'
+const defaultStyles = 'bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-sm text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-indigo-400'
 
 /**
  *
