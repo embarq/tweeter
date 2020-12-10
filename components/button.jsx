c91c535e5a52fe098cd15a003ec6398bcae205c8
embarq <embarq.dev@gmail.com>
2020-12-10 10:43:02 +0200

    refactor button: declare & use styles map

--- components/button.jsx
+++ components/button.jsx
@@ -1,13 +1,14 @@
 import classNames from 'classnames'
 import Loader from './loader'
 
-const defaultStyles = 'btn hover:bg-indigo-500 focus:shadow-outline focus:outline-none'
-const clearStyles = 'btn-clear hover:text-gray-700 hover:bg-gray-100 focus:shadow-outline focus:outline-none'
+const ButtonStyles = {
+  primary: 'btn hover:bg-indigo-500 focus:shadow-outline focus:outline-none',
+  clear: 'btn-clear hover:text-gray-700 hover:bg-gray-100 focus:shadow-outline focus:outline-none',
+}
 
-export default function Button({className, children, kind, loading, ...props}) {
-  const buttonStyle = kind === 'clear' ? clearStyles : defaultStyles
+export default function Button({className, children, kind = 'primary', loading, ...props}) {
   return (
-    <button {...props} className={classNames(buttonStyle, className)} disabled={loading}>
+    <button {...props} className={classNames(ButtonStyles[kind], className)} disabled={loading}>
       {loading && <div className="w-4 h-4 mr-2"><Loader /></div>}
       {children}
     </button>
