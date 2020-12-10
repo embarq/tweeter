767aa21cb27725c2fa6803c8148163ec4e38da1d
embarq <embarq.dev@gmail.com>
2020-12-10 10:43:22 +0200

    update form-field labels on under md breakpoint

--- components/form-field.jsx
+++ components/form-field.jsx
@@ -4,7 +4,7 @@ export default function FormField({ children, label, labelFor, className }) {
   return (
     <div className={classNames('md:flex md:items-center mb-6', className)}>
       <div className="md:w-1/3">
-        <label className="text-sm block text-gray-500 md:text-right mb-1 md:mb-0 pr-4" htmlFor={labelFor}>
+        <label className="text-sm block text-gray-500 md:text-right mb-1 md:mb-0 pr-4 pl-2 md:pl-0" htmlFor={labelFor}>
           {label}
         </label>
       </div>
