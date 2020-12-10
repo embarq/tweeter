e1823664e5ba6958ccfd22ddc10709d0eb4e9b04
embarq <embarq.dev@gmail.com>
2020-12-10 10:43:35 +0200

    align modal to top

--- components/modal.jsx
+++ components/modal.jsx
@@ -1,7 +1,7 @@
 export default function Modal({ children, onDismiss, title }) {
   return (
     <div className="fixed z-10 inset-0 overflow-y-auto">
-      <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+      <div className="flex items-start justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
         <div className="fixed inset-0 transition-opacity">
           <div className="absolute inset-0 bg-gray-300 opacity-75"></div>
         </div>
