8be23e1fdfa50e6ba20ad2075dc1540a1d4d0919
embarq <embarq.dev@gmail.com>
2020-12-10 10:00:08 +0200

    fix user name display; update text sizes & colors

--- components/header.jsx
+++ components/header.jsx
@@ -16,7 +16,7 @@ const LINKS = [
 export default function Header() {
   const [showNavPopover, setShowNavPopover] = useState(false)
   const { pathname } = useRouter()
-  const { user } = useRecoilValue(state.userProfile)
+  const user = useRecoilValue(state.userProfile)
   const links = LINKS.map((link, i, arr) => (
     <HeaderNavItem
       key={link.text}
@@ -27,14 +27,15 @@ export default function Header() {
 
   return (
     <header className="fixed top-0 w-full bg-white shadow-sm">
-      <div className="container mx-auto flex items-center justify-between">
+      <div className="container relative mx-auto flex items-center justify-between">
         <strong className="text-indigo-500">Tweeter</strong>
         <nav className="flex list-none pt-2">
           {links}
         </nav>
-        <Button onClick={() => setShowNavPopover(true)} kind="clear">
-          <div className="text-indigo-500">{user && user.fullName}</div>
-          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6.34317 7.75732L4.92896 9.17154L12 16.2426L19.0711 9.17157L17.6569 7.75735L12 13.4142L6.34317 7.75732Z" fill="currentColor" /></svg>
+        <div className="noop"></div>
+        <Button onClick={() => setShowNavPopover(true)} kind="clear" className="absolute right-0">
+          <div className="text-indigo-400 mr-2 text-sm font-bold">{user && user.fullname}</div>
+          <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6.34317 7.75732L4.92896 9.17154L12 16.2426L19.0711 9.17157L17.6569 7.75735L12 13.4142L6.34317 7.75732Z" fill="currentColor" /></svg>
         </Button>
       </div>
       {showNavPopover &&
