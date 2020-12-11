0bdc740947decec7f939a17652d8bebd6cfdc1d2
embarq <embarq.dev@gmail.com>
2020-12-11 13:06:21 +0200

    fix mobile styles

--- components/me-page-layout.jsx
+++ components/me-page-layout.jsx
@@ -3,13 +3,13 @@ import { myPagesLinks } from '../constants/ui'
 
 export default function MePageLayout({ children }) {
   return (
-    <div className="container mx-auto pt-6 flex h-full">
-      <div className="w-1/4 px-4">
+    <div className="container mx-auto md:pt-6 md:flex h-full px-4 md:px-0">
+      <div className="hidden md:block w-1/4 px-4">
         <div className="p-4 w-3/4">
           <MePageNav links={myPagesLinks} />
         </div>
       </div>
-      <div className="w-3/4">
+      <div className="w-full md:w-3/4">
         <div className="card h-full">
           {children}
         </div>
