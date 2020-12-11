b013ce54adb161704c9527bc253c2dfe43906a16
embarq <embarq.dev@gmail.com>
2020-12-11 12:15:28 +0200

    display user avatar; impl mobile design

--- components/header.jsx
+++ components/header.jsx
@@ -1,12 +1,14 @@
 import { useRouter } from 'next/router'
 import Link from 'next/link'
 import { useState } from 'react'
+import { useRecoilValue } from 'recoil'
+import classNames from 'classnames'
 import HeaderNavItem from './header-nav-item'
 import Button from './button'
 import * as state from '../constants/state'
 import HeaderNavPopover from './header-nav-popover'
-import { useRecoilValue } from 'recoil'
 import { logo as logoIcon } from '../util/icons'
+import UserAvatar from './user-avatar'
 
 const LINKS = [
   { link: '/', text: 'Home' },
@@ -29,21 +31,30 @@ export default function Header() {
 
   return (
     <header className="fixed top-0 w-full bg-white shadow-sm">
-      <div className="container relative mx-auto flex items-center justify-between">
+      <div className="container relative mx-auto h-16 flex items-center justify-between px-4 md:px-0">
         <Link href="/">
           <a className="flex align-center text-indigo-500">
             {logoIcon}
             <span className="ml-3 pt-1 font-bold">Tweeter</span>
           </a>
         </Link>
-        <nav className="flex list-none pt-2">
+        <nav className="hidden md:flex list-none">
           {links}
         </nav>
-        <div className="noop"></div>
-        <Button onClick={() => setShowNavPopover(true)} kind="clear" className="absolute right-0">
-          <div className="text-indigo-400 mr-2 text-sm font-bold">{user && user.fullname}</div>
-          <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6.34317 7.75732L4.92896 9.17154L12 16.2426L19.0711 9.17157L17.6569 7.75735L12 13.4142L6.34317 7.75732Z" fill="currentColor" /></svg>
-        </Button>
+        <div className="md:flex md:justify-end md:w-48 h-full">
+        	<Button
+        	  onClick={() => setShowNavPopover(true)}
+        	  className="flex justify-end h-full px-2 border-b-4 border-transparent focus:shadow-none"
+        	  kind="clear">
+        	  <UserAvatar avatarId={user.avatar} className="mr-3 md:mx-0" />
+        	  <div className="hidden md:inline ml-3 mx-2 text-indigo-400 text-right text-xs font-bold">{user && user.fullname}</div>
+        	  <svg
+        	    width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"
+        	    className={classNames('transform transition-transform duration-100', { 'rotate-180': showNavPopover })}>
+        	    <path d="M6.34317 7.75732L4.92896 9.17154L12 16.2426L19.0711 9.17157L17.6569 7.75735L12 13.4142L6.34317 7.75732Z" fill="currentColor" />
+        	  </svg>
+        	</Button>
+        </div>
       </div>
       {showNavPopover &&
         <HeaderNavPopover onDismiss={() => setShowNavPopover(false)} />
