1515d8b790dad795b0f846866ede7f18ad3f13a5
embarq <embarq.dev@gmail.com>
2021-01-03 09:55:07 +0200

    remove rounded corners

--- components/app-loading.jsx
+++ components/app-loading.jsx
@@ -2,7 +2,7 @@ import Loader from './loader'
 
 export default function AppLoading() {
   return (
-    <div className="flex flex-col justify-center items-center h-full text-center rounded-lg bg-indigo-400 text-white">
+    <div className="flex flex-col justify-center items-center h-full text-center bg-indigo-400 text-white">
       <h1 className="text-4xl font-bold">Tweeter</h1>
       <div className="w-1/6 p-4 flex flex-row justify-center items-center">
         <div className="w-6 h-6">
